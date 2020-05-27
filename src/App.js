6b5056d1a373d0c0df034448c450d06e786a1621
GiovanniDw <giovannidwidjosewojo@hotmail.com>
2020-05-27 04:07:08 +0200

    added styled components and global style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import GlobalStyle from './GlobalStyle'
 import Tabs from './components/Tabs/Tabs';
 import Home from './pages/Home';
 import Courses from "./pages/Courses";
+import courseList from "./courses-data.json";
 
 const App = () => {
   return (
 		<Router>
-			<Tabs />
+			<GlobalStyle />
 			<Switch>
-				<Route path="/courses" component={Courses} />
+				<Route
+					path="/courses"
+					component={Courses}
+					courses={courseList}
+				/>
 				<Route path="/" component={Home} />
 			</Switch>
+			<Tabs />
 		</Router>
   );
 }
