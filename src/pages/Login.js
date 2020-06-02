b85dfa7857ae5989cbbd8ad27f3bc35a2c95bf5c
ralfz123 <ralf_zonneveld@hotmail.com>
2020-06-02 16:18:12 +0200

    Added content to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,10 +4,48 @@ import Header from "../components/Header/Header";
 import Content from "../components/Content/Content";
 
 class Login extends Component {
-  render() {
-      return (
+  constructor(props) {
+    super(props);
 
-      )
+    this.state = {
+      email: "",
+      password: "",
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    const { email, password } = this.state;
+  }
+
+  render() {
+    return (
+      <div>
+        <Header>{/* <Title name={`Login`} /> */}</Header>
+        <Content>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={this.state.email}
+              required
+            />
+
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              value={this.state.password}
+              required
+            />
+
+            <button type="submit">Login</button>
+          </form>
+        </Content>
+      </div>
+    );
   }
 }
 
