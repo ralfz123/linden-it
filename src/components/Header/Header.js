ad0876c4580d84aa05dc31c0ff2cbcbb4134b09f
ralfz123 <ralf_zonneveld@hotmail.com>
2020-06-02 16:21:33 +0200

    Set header to sticky, so you see the body content

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 
 const StyledHeader = styled.header`
 	position: fixed;
-	/* position: -webkit-sticky;
-	position: sticky; */
+	/* position: -webkit-sticky; */
+	position: sticky; 
 	left: 0;
 	right: 0;
 	top: 0;
