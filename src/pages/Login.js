67d6585d5c712624bf9fe8730b03e069f9811cc1
ralfz123 <ralf_zonneveld@hotmail.com>
2020-06-02 15:18:12 +0200

    Added Login page

---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Title from "../components/Title/Title";
+import Header from "../components/Header/Header";
+import Content from "../components/Content/Content";
+
+class Login extends Component {
+  render() {
+      return (
+
+      )
+  }
+}
+
+export default Login;
