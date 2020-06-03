27397e5ea452136b0fbcc866ad0ae3e172a33e07
ralfz123 <ralf_zonneveld@hotmail.com>
2020-06-03 15:44:42 +0200

    Add input field component

---
+++ src/components/Form/StyledInput.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const StyledInput = styled.input `
+	position: fixed;
+	/* position: -webkit-sticky;*/
+	left: 0;
+	right: 0;
+	top: 0;
+	width: 100vw;
+	background-color: var(--color-primary);
+	color: var(--color-light);
+.header-toolbar {
+    min-height: 50px;
+}
+`
+export default StyledInput;
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from "react";
 // import PropTypes from 'prop-types';
+// import StyledInput from "./StyledInput";
 
 const FormInput = ({
-    name,
-    type,
-    placeholder,
-    onChange,
-    className,
-    value,
-    error,
-    children,
-    label,
-    ...props
-  }) => {
-    
-    return (
+  name,
+  type,
+  placeholder,
+  onChange,
+  className,
+  value,
+  error,
+  children,
+  label,
+  ...props
+}) => {
+  return (
+    // <StyledInput>
       <React.Fragment>
         <label htmlFor={name}>{label}</label>
         <input
@@ -25,26 +26,27 @@ const FormInput = ({
           onChange={onChange}
           value={value}
           className={className}
-          style={error && {border: 'solid 1px red'}}
+          style={error && { border: "solid 1px red" }}
         />
-        { error && <p>{ error }</p>}
+        {error && <p>{error}</p>}
       </React.Fragment>
-    )
-  }
-  
-  FormInput.defaultProps = {
-    type: "text",
-    className: ""
-  }
-  
-  // FormInput.propTypes = {
-  //   name: PropTypes.string.isRequired,
-  //   type: PropTypes.string,
-  //   placeholder: PropTypes.string.isRequired,
-  //   type: PropTypes.oneOf(['text', 'number', 'password']),
-  //   className: PropTypes.string,
-  //   value: PropTypes.any,
-  //   onChange: PropTypes.func.isRequired
-  // }
+    // </StyledInput>
+  );
+};
 
-  export default FormInput
+FormInput.defaultProps = {
+  type: "text",
+  className: "",
+};
+
+// FormInput.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   type: PropTypes.string,
+//   placeholder: PropTypes.string.isRequired,
+//   type: PropTypes.oneOf(['text', 'number', 'password']),
+//   className: PropTypes.string,
+//   value: PropTypes.any,
+//   onChange: PropTypes.func.isRequired
+// }
+
+export default FormInput;
