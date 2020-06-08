711f2394d9e2673517891df1c81eaeb20c69d5d0
ralfz123 <ralf_zonneveld@hotmail.com>
2020-06-08 10:51:51 +0200

    Added error boundaries to login page, but is still not working

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,83 +9,137 @@ import Button from "../components/Button";
 import { Link } from "react-router-dom";
 // import { AiOutlineEye } from 'react-icons/ai';
 
+// const initialState = {
+// 	email: "",
+// 	password: "",
+// 	title: "Inloggen",
+// 	emailError: "",
+// 	passwordError: "",
+// }
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-      title: "Inloggen",
-    };
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: "",
+			password: "",
+			title: "Inloggen",
+			emailError: "",
+			passwordError: "",
+		};
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.validate = this.validate.bind(this);
+	}
 
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  }
+	handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	}
 
-  handleSubmit(event) {
-    const { email, password } = this.state;
-  }
+	validate = () => {
+		let emailError = "";
+		let passwordError = "";
 
-  render() {
-    return (
-		<>
-			<Header>
-				<Title title={this.state.title} />
-			</Header>
-			<Content className='login-wrapper'>
-				<form onSubmit={this.handleSubmit} className='login' action='/'>
-					<div className='field'>
-						<label for='email'>E-mail</label>
-						<Input
-							label='Email'
-							name='email'
-							type='email'
-							onChange={this.handleChange}
-							placeholder='E-mail'
-							value={this.state.email}
-							required
-						/>
-					</div>
+		// email error
+		if (!this.state.email.includes("@")) {
+			emailError = "Email is incorrect";
+			console.log("Bevat geen '@' !");
+		}
 
-					<div className='field'>
-						<label for='password'>Wachtwoord</label>
-						<Input
-							label='Wachtwoord'
-							name='password'
-							type='password'
-							onChange={this.handleChange}
-							placeholder='Wachtwoord'
-							value={this.state.password}
-							minLength= '6'
-							required
-						/>
-						{/* <i className='eye password-icon'></i> */}
-						{/* <AiOutlineEye className="eye password-icon" /> */}
+		// password error
+		if (this.state.password < 6) {
+			passwordError = "Wachtwoord moet minimaal 6 tekens bevatten";
+		}
 
-						<Link className='link pw-forgot' to='/reset-password'>
-							Wachtwoord vergeten?
-						</Link>
-					</div>
+		if (emailError || passwordError) {
+			this.setState({ emailError, passwordError });
+			return false;
+		}
+		return true;
+	};
 
-					<Button className='logbutt' type='submit'>
-						Inloggen
-					</Button>
+	handleSubmit(event) {
+		const { email, password } = this.state;
+		const isValid = this.validate();
+		if (isValid) {
+			console.log(this.state);
+			// clear form
+			this.setState(this.state);
+		}
+	}
 
-					<p className='privacy'>
-						Je gaat akkoord met het Privacy Statement van Linden-IT
-					</p>
-				</form>
-				{/* <reCAPTCHA sitekey="Your client site key" onChange={onChange} /> */}
-			</Content>
-		</>
-	);
-  }
+	render() {
+		return (
+			<>
+				<Header>
+					<Title title={this.state.title} />
+				</Header>
+				<Content className='login-wrapper'>
+					<form
+						onSubmit={this.handleSubmit}
+						className='login'
+						// action='/login'
+					>
+						<div style={{ fontSize: 12, color: "red" }}>
+							{this.state.emailError}
+						</div>
+
+						<div style={{ fontSize: 12, color: "red" }}>
+							{this.state.passwordError}
+						</div>
+
+						<div className='field'>
+							<label for='email'>E-mail</label>
+							<Input
+								label='Email'
+								name='email'
+								// type='email'
+								onChange={this.handleChange}
+								placeholder='E-mail'
+								value={this.state.email}
+								required
+							/>
+						</div>
+
+						<div className='field'>
+							<label for='password'>Wachtwoord</label>
+							<Input
+								label='Wachtwoord'
+								name='password'
+								// type='password'
+								onChange={this.handleChange}
+								placeholder='Wachtwoord'
+								value={this.state.password}
+								minLength='6'
+								required
+							/>
+							{/* <i className='eye password-icon'></i> */}
+							{/* <AiOutlineEye className="eye password-icon" /> */}
+
+							<Link
+								className='link pw-forgot'
+								to='/reset-password'
+							>
+								Wachtwoord vergeten?
+							</Link>
+						</div>
+
+						<Button className='logbutt' type='button'>
+							Inloggen
+						</Button>
+
+						<p className='privacy'>
+							Je gaat akkoord met het Privacy Statement van
+							Linden-IT
+						</p>
+					</form>
+					{/* <reCAPTCHA sitekey="Your client site key" onChange={onChange} /> */}
+				</Content>
+			</>
+		);
+	}
 }
 
 export default Login;
