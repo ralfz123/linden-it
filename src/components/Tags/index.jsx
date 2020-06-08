52374d6e47495ba6b20f43f773c468749f56916c
ralfz123 <ralf_zonneveld@hotmail.com>
2020-06-08 15:07:43 +0200

    (Code syntax) Changed components into arrow functions

--- src/components/Tags/index.jsx
+++ src/components/Tags/index.jsx
@@ -11,20 +11,6 @@ position:absolute;
     font-size:12px;
 `;
 
-// class Tags extends Component {
-//     render() {
-//         return (
-//               <Tag>{this.props.children}</Tag>  
-//         );
-//     }
-// }
-
-
-// Tags.propTypes = {
-
-// };
-
-
 export const Tags = ({ children }) => {
     return (
         <Tag>{children}</Tag>  
--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,16 +1,6 @@
 import React, { Component } from 'react';
 import StyledHeading from './StyledHeading.js'
 
-// class Title extends Component {
-// 	render() {
-// 		return (
-// 			<StyledHeading>
-// 				{this.props.title}
-// 			</StyledHeading>
-// 		);
-// 	}
-// }
-
 export const Title = ({ title }) => {
 	return (
 		<StyledHeading>
