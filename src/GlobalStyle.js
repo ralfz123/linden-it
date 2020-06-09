d92735c7ca2403d3019a8f5e4bf15a8a27f6264c
GiovanniDw <giovannidwidjosewojo@hotmail.com>
2020-06-09 10:26:42 +0200

    removed old sement styles

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -11,7 +11,7 @@ export const colors = {
 };
 
 const GlobalStyle = createGlobalStyle`
-    ${normalize}
+    ${normalize};
     :root {
         --color-primary: #FC7928;
         --color-light: #ffffff;
@@ -31,7 +31,7 @@ const GlobalStyle = createGlobalStyle`
         background-color: var(--color-primary);
     }
     *, *:before, *:after {
-        box-sizing: inherit;
+        box-sizing: border-box;
     }
     body, #root {
         height:100%;
@@ -161,44 +161,7 @@ ${"" /* Login page styles */}
     i:hover {
         color: ${colors.primary};
          cursor: pointer;
-}
-
-${"" /* Segmented controll */}
-.r-segmented-control {
-    border-bottom:0;
-
- ul {
-     
-    background-color:var(--color-gray-light);
-    border-radius:5px;
-    height:40px;
-
-    li {
-        border:0px solid black;
-        background-color:var(--color-gray-light);
-        border-radius:0;
-        color:var(--color-gray-dark);
     }
-
-    li.segment {
-        height:auto;
-        font-size:11px;
-        ${"" /* border-right: 1px solid var(--color-grayLightTwo); */}
-    }
-    li.segment.selected {
-        background-color:var(--color-light);
-        border-radius: 5px;
-        box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
-        transition:.5s;
-        z-index:1;
-        font-weight:700;
-
-    }
-}
-}
-.r-segmented-control > ul > li:last-of-type {
-    border-right:0;
-}
 `;
 
 export default GlobalStyle;
