f65036986ef8ecb9ccfa0016177151ddb66c13a2
ralfz123 <ralf_zonneveld@hotmail.com>
2020-06-12 12:59:19 +0200

    Added a 🆕  page - SettingGoals

---
+++ src/pages/SettingGoals.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import Content from '../components/Content/Content';
+import Title from '../components/Title';
+import { PrimaryButton } from '../components/Button';
+import { SecondaryButton } from '../components/Button';
+
+class Goals extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: 'Mijn agenda',
+		};
+	}
+
+	render() {
+		return (
+			<>
+				<Header>
+					<Title title={this.state.title} />
+				</Header>
+				<Content>
+					<p>Agenda Content</p>
+					<PrimaryButton label='Dit is een primaire button' />
+					<SecondaryButton label='Dit is een secundaire button' />
+				</Content>
+			</>
+		);
+	}
+}
+export default Goals;
